b6381e793b95e2a128678fe36ef371c27d285de4
XYZ <XYZ@DESKTOP-U8F78NA>
2020-02-05 18:56:00 +0530

    spring_basic

---
+++ Spring Basic/src/com/spring/basic/Student.java
@@ -0,0 +1,16 @@
+package com.spring.basic;
+
+public class Student 
+{
+
+	public Student() 
+	{
+		System.out.println("Student object created");
+	}
+	void study()
+	{
+		System.out.println("student Study");
+	}
+	
+
+}
